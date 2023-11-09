051f5d2c676e3f72a3ee45a93f1d5eddf6d3f929
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2023-11-09 15:04:21 +0100

    Install SVG jar & seedrandom

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    svgJar: {
+      sourceDirs: ['public/icons'],
+    },
   });
 
   app.import('node_modules/modern-normalize/modern-normalize.css');
